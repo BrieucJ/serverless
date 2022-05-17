3e85baff7e6f3efad1793de740a703e6dd6ebe40
Brieuc Jamain <brieucjamain@gmail.com>
2022-05-17 22:47:06 +0200

    lambda change

--- src/lambda.ts
+++ src/lambda.ts
@@ -10,15 +10,14 @@ import { Request } from 'express'
 import { Context } from './utils/types'
 
 logger.info('Lambda started')
-
-exports.handler = async () => {
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions */
+const main = async () => {
   await source.initialize()
   logger.info('Datasource initialized')
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
     context: async ({ event, context, express }: { event: any; context: any; express: any }): Promise<Context> => {
-      /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/restrict-template-expressions */
       // logger.info(`event ${event}`)
       // logger.info(`context ${context}`)
       // logger.info(`express ${express}`)
@@ -34,16 +33,6 @@ exports.handler = async () => {
   return apolloServer.createHandler()
 }
 
-// let server
-
-// main()
-//   .then((apolloServer) => {
-//     exports.handler = apolloServer.createHandler()
-//     logger.info('Server up')
-//   })
-//   .catch((error: any) => {
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-//     logger.error('ERROR initializing Datasource', { error })
-//   })
-
-// exports.handler = server
+exports.handler = (async () => {
+  await main()
+})()
